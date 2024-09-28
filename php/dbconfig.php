e03bd1199e5bb43fa85f20759fef1e16d1b5ddfc
rohit3021 <rohit302108@gmail.com>
2024-09-29 00:16:54 +0530

    new_db_cfg

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,6 +1,10 @@
 <?php
 // dbconfig.php
 
+// $servername = "sql201.infinityfree.com"; // Use your database server details
+// $username = "if0_37405908";        // Your MySQL username
+// $password = "OX2Du3ov2OvnPka";            // Your MySQL password (if any)
+// $dbname = "if0_37405908_blog_app";      // Your database name
 $servername = "localhost"; // Use your database server details
 $username = "root";        // Your MySQL username
 $password = "";            // Your MySQL password (if any)
